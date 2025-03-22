0d68fe547e3774a660610dff3a0247b1aeb073b4
Jonas Geiler <git@jonasgeiler.com>
2025-03-22 12:55:49 +0100

    chore: init with current type definition

---
+++ README.md
@@ -0,0 +1,11 @@
+# lua-fenster-audio type definitions
+
+This repository contains the type definitions for [lua-fenster-audio](https://github.com/jonasgeiler/lua-fenster-audio)
+that can be used by [Sumneko's Lua Language Server](https://github.com/LuaLS/lua-language-server).
+
+It is a submodule for the [LuaLS/LLS-Addons](https://github.com/LuaLS/LLS-Addons) repository.
+
+## Usage
+
+Please consult the documentation for more information:
+[Installing Addons - Lua Language Server | Wiki](https://luals.github.io/wiki/addons/#installing-addons)
---
+++ library/fensteraudio.lua
@@ -0,0 +1,57 @@
+---@meta
+--[[
+
+Type definitions for lua-fenster-audio
+https://github.com/jonasgeiler/lua-fenster-audio
+https://luarocks.org/modules/jonasgeiler/fensteraudio
+
+--]]
+
+---@class audiodevice*
+local audiodevice = {}
+
+function audiodevice:close() end
+
+---@return integer
+---@nodiscard
+function audiodevice:available() end
+
+---@param data number[]
+function audiodevice:write(data) end
+
+---@param key string
+---@return any
+---@nodiscard
+function audiodevice:__index(key) end
+
+function audiodevice:__gc() end
+
+---@version >5.4
+function audiodevice:__close() end
+
+---@return string
+---@nodiscard
+function audiodevice:__tostring() end
+
+---@class fensteraudio
+---@field samplerate integer
+---@field buffersize integer
+local fensteraudio = {}
+
+---@return audiodevice*
+---@nodiscard
+function fensteraudio.open() end
+
+---@param audiodevice audiodevice*
+function fensteraudio.close(audiodevice) end
+
+---@param audiodevice audiodevice*
+---@return integer
+---@nodiscard
+function fensteraudio.available(audiodevice) end
+
+---@param audiodevice audiodevice*
+---@param data number[]
+function fensteraudio.write(audiodevice, data) end
+
+return fensteraudio
